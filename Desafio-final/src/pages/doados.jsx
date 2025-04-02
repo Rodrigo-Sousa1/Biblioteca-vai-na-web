938f31500b8f8bce9f67d99e9649e0cfa0401a26
Rodrigo-Sousa1 <rodrigo-s-lima1@gmail.com>
2025-04-02 19:22:53 -0300

    exibindo dados recebidos da api

--- Desafio-final/src/pages/doados.jsx
+++ Desafio-final/src/pages/doados.jsx
@@ -1,7 +1,28 @@
 import LIvro from '../assets/imagem/livrodoado.jpg'
 import s from './doados.module.scss'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 export default function Doados() {
+ const [livros, setLivros] = useState([])
+ 
+     const getLivros = async () => {
+         try {
+             const response = await axios.get("https://api-livro-2.onrender.com/livros")
+             setLivros(response.data)
+         } catch (error) {
+             console.error("Erro ao buscar livros:", error)
+         }
+     }
+
+     useEffect(() => {
+         getLivros()
+     }, [])
+
+
     return (
+
+
         <main className={s.mainDoados}>
             <h2>Livros Doados </h2>
 
@@ -13,6 +34,16 @@ export default function Doados() {
                 <p>Susanne Andrade</p>
                 <p>Ficção</p>
                 </div>
+
+                 {livros.map((item) => (
+                     <article key={item.id}>
+                         <img src={item.imagem_url} alt={item.titulo} />
+                         <h3>{item.titulo}</h3>
+                         <p>{item.autor}</p>
+                         <p>{item.categoria}</p>
+                     </article>
+                 ))}
+
             </section>
 
         </main>
